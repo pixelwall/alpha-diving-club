e2ec5cee4ccbdcce855182d7b5e1f4f8aec562c3
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-06-27 20:07:47 +0000

    Add missing animations

--- components/www/home/card.tsx
+++ components/www/home/card.tsx
@@ -5,24 +5,29 @@ import Parallax from '@/components/parallax'
 
 const Card = ({ props }) => (
 	<div className='shadow-md mx-auto my-4 lg:my-12 transform hover:scale-110 duration-200 hover:cursor-pointer'>
-		{/* <Viewport
+		<Viewport
+			className='pr-2 animate'
+			style={setAnim({ x: '-1rem', d: '800ms' })}
+		>
+			{/* <Viewport
 			className='flex flex-col mr-0 mb-16 w-full animate justify-center lg:justify-end relative sm:w-1/2 lg:mb-0'
 			style={setAnim({ x: '1rem', y: '1rem', d: '200ms' })}
 		> */}
-		<Image
-			src={`/images/${props.image}`}
-			alt=''
-			width={300}
-			height={200}
-			objectFit='cover'
-			layout='intrinsic'
-		/>
-		<div className='mx-4 mt-3 mb-4'>
-			<h2 className='text-2xl font-extrabold'>{props.name}</h2>
-			<p>{props.description}</p>
-			<h2 className='text-2xl font-extrabold text-right'>{props.price}</h2>
-		</div>
-		{/* </Viewport> */}
+			<Image
+				src={`/images/${props.image}`}
+				alt=''
+				width={300}
+				height={200}
+				objectFit='cover'
+				layout='intrinsic'
+			/>
+			<div className='mx-4 mt-3 mb-4'>
+				<h2 className='text-2xl font-extrabold'>{props.name}</h2>
+				<p>{props.description}</p>
+				<h2 className='text-2xl font-extrabold text-right'>{props.price}</h2>
+			</div>
+			{/* </Viewport> */}
+		</Viewport>
 	</div>
 )
 
--- components/www/home/largeCard.tsx
+++ components/www/home/largeCard.tsx
@@ -8,23 +8,28 @@ const LargeCard = ({ props }) => (
 	<div
 		className={`${styles['largeCard']} shadow-md mx-auto my-8 lg:my-12 transform hover:scale-110 duration-200 hover:cursor-pointer`}
 	>
-		{/* <Viewport
+		<Viewport
+			className='pr-2 animate'
+			style={setAnim({ x: '-1rem', d: '800ms' })}
+		>
+			{/* <Viewport
 			className='flex flex-col mr-0 mb-16 w-full animate justify-center lg:justify-end relative sm:w-1/2 lg:mb-0'
 			style={setAnim({ x: '1rem', y: '1rem', d: '200ms' })}
 		> */}
-		<div className={`${styles['image']}`}>
-			<Image
-				src={`/images/${props.image}`}
-				alt=''
-				width={451}
-				height={291}
-				objectFit='cover'
-			/>
-		</div>
-		<div className='w-full lg:h-full px-4 pt-6 pb-4'>
-			<h2 className='mb-2 text-xl font-black'>{props.name}</h2>
-			<p className='leading-relaxed text-md'>{props.description}</p>
-		</div>
+			<div className={`${styles['image']}`}>
+				<Image
+					src={`/images/${props.image}`}
+					alt=''
+					width={451}
+					height={291}
+					objectFit='cover'
+				/>
+			</div>
+			<div className='w-full lg:h-full px-4 pt-6 pb-4'>
+				<h2 className='mb-2 text-xl font-black'>{props.name}</h2>
+				<p className='leading-relaxed text-md'>{props.description}</p>
+			</div>
+		</Viewport>
 		{/* </Viewport> */}
 	</div>
 )
