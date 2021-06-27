806b4e36071ac581e10e086bd52fc300dfee84ce
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-06-26 23:32:05 -0300

    Add quote

--- components/www/home/index.tsx
+++ components/www/home/index.tsx
@@ -3,6 +3,7 @@ import Hero from './hero'
 import HeroInverted from './heroInverted'
 import HeroCenter from './heroCenter'
 import CardContainer from './cardContainer'
+import Quote from './quote'
 
 export type IndexProps = PageProps
 
@@ -17,6 +18,7 @@ const Index = (data: IndexProps) => (
 		<HeroInverted />
 		<HeroCenter />
 		<CardContainer />
+		<Quote />
 	</>
 )
 
