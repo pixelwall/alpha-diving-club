a79a5a298ff730424e330e1fd1a11c928895f231
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-06-27 12:49:22 +0000

    Push test

--- components/www/home/largeCard.tsx
+++ components/www/home/largeCard.tsx
@@ -4,7 +4,7 @@ import Viewport, { setAnim } from '@/components/viewport'
 import Parallax from '@/components/parallax'
 
 const LargeCard = ({ props }) => (
-	<div className='w-60 shadow-md mx-auto my-8 lg:my-12'>
+	<div className='shadow-md mx-auto my-8 lg:my-12'>
 		{/* <Viewport
 			className='flex flex-col mr-0 mb-16 w-full animate justify-center lg:justify-end relative sm:w-1/2 lg:mb-0'
 			style={setAnim({ x: '1rem', y: '1rem', d: '200ms' })}
