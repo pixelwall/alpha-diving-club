cb1c10b763af161a6cd0c92b9c0b0301d5c61f2c
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-07-18 17:03:33 +0000

    Home: add 2nd custom image

--- components/www/home/heroInverted.tsx
+++ components/www/home/heroInverted.tsx
@@ -8,27 +8,27 @@ const HeroInverted = () => (
 		<div className='flex min-h-screen pt-20 xl:pt-0 c'>
 			<div className='flex flex-wrap-reverse my-auto w-full items-center sm:flex-wrap'>
 				<Viewport
-					className='flex mb-16 w-full animate justify-center lg:justify-end relative sm:w-1/2 lg:mb-0'
+					className='flex mb-16 w-full animate justify-center lg:justify-start relative sm:w-1/2 lg:mb-0'
 					style={setAnim({ x: '1rem', y: '1rem', d: '200ms' })}
 				>
-					<div
+					{/* <div
 						className='h-full animate w-7/10 -z-10 absolute lg:h-80 lg:w-80'
 						style={setAnim({ x: '-1.25rem', y: '-1.25rem', d: '0.2s' })}
 					>
 						<div className='h-full rounded-[50%] w-full transform translate-x-5 translate-y-5 polka' />
-					</div>
-					<div className='flex bg-blue-300 bg-opacity-50 rounded-[50%] w-7/10 items-center justify-center lg:h-80 lg:w-80'>
-						<Parallax negative className='p-4'>
-							<Image
-								src='/images/arepa.png'
-								alt=''
-								width={481}
-								height={481}
-								objectFit='contain'
-								layout='intrinsic'
-							/>
-						</Parallax>
-					</div>
+					</div> */}
+					{/* <div className='flex bg-blue-300 bg-opacity-50 rounded-[50%] w-7/10 items-center justify-center lg:h-80 lg:w-80'> */}
+					{/* <Parallax negative className='p-4'> */}
+					<Image
+						src='/images/grouppositionCustom.png'
+						alt=''
+						width={481}
+						height={581}
+						// objectFit='contain'
+						layout='intrinsic'
+					/>
+					{/* </Parallax> */}
+					{/* </div> */}
 				</Viewport>
 				<div className='w-full sm:w-1/2'>
 					<Viewport
