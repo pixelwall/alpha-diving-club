913810a20687145031e26a680f33e0e3cc1000fc
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-07-25 12:21:15 +0000

    About: fix first image's responsiveness

--- components/www/about/heroBackgroundImage.tsx
+++ components/www/about/heroBackgroundImage.tsx
@@ -3,10 +3,10 @@ import styles from './styles/backgroundImage.module.css'
 
 const HeroBackgroundImage = () => (
 	<div
-		className={`${styles['wallpaper']} flex mb-4 h-96 w-full px-0 pt-8 lg:pt-20 xl:pt-0 c`}
+		className={`${styles['wallpaper']} flex mb-4 w-full px-0 pt-8 lg:pt-20 xl:pt-0 c`}
 	>
 		<Viewport
-			className='text-white my-auto sm:w-5/12 flex flex-col ml-28 lg:mr-32 animate'
+			className='text-white mt-32 md:my-auto sm:w-5/12 flex flex-col mx-8 md:ml-28 lg:mr-32 animate'
 			style={setAnim({ x: '-1rem', d: '800ms' })}
 		>
 			<h1 className='font-medium text-3xl xl:text-5xl xl:leading-tight'>
