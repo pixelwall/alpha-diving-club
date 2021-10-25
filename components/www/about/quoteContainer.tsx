e957c5c3ef4efe01c36e87bad55cc310c8b0e179
Ettore J. Bortolin <ettorej.bortolin@gmail.com>
2021-10-25 21:50:36 +0000

    About: Fix info

--- components/www/about/quoteContainer.tsx
+++ components/www/about/quoteContainer.tsx
@@ -7,13 +7,13 @@ const quoteInfo = [
 	{
 		image: 'diveCustom.png',
 		title: 'Seattle, Washington. USA',
-		body: `We arrived in Seattle, Washington on September of 2015 and for the past 3 years we have been building divers from the ground up. Using safe and methodologically planned workouts.
+		body: `We arrived in Seattle, Washington on September of 2015 and for the past 6 years we have been building divers from the ground up. Using safe and methodologically planned workouts.
         In 2018, 8 of our divers made the qualification to USA Diving Junior Olympic Nationals.`,
 	},
 	{
 		image: 'rollingCustom.png',
 		title: 'Venezuela',
-		body: `Our story started in 1999 while living in Venezuela. Alejandra Fuentes was preparing for the 2000 Sydney Olimpic Games and Ilich Marcano was preparing the next generation of champion. Time went by and divers such as Robert Paez and Maria Betancourt became the result of all the hard work placed throughout many years.`,
+		body: `Our story started in 1999 while living in Venezuela. Alejandra Fuentes was preparing for the 2000 Sydney Olimpic Games and Ilich Marcano was preparing the next generation of champions. Time went by and divers such as Robert Paez and Maria Betancourt became the result of all the hard work placed throughout many years.`,
 		footer: '',
 	},
 ]
@@ -21,11 +21,7 @@ const quoteInfo = [
 const QuoteContainer = () => (
 	<div className='flex flex-col md:flex-row mx-12'>
 		<QuoteImageTop props={quoteInfo[0]} />
-		<img
-			src={'/images/blueline.png'}
-			alt=''
-			className={`${styles['image']}`}
-		/>
+		<img src={'/images/blueline.png'} alt='' className={`${styles['image']}`} />
 		{/* <Image
 			src={'/images/blueline.png'}
 			alt=''
